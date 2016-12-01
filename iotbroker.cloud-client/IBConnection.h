daecc04a50a0c6bb437505db9429a6d9b09928ac
MacOS <macos@iMac-MacOS.local>
2016-12-01 15:55:56 +0200

    Initial commit

---
+++ iotbroker.cloud-client/IBConnection.h
@@ -0,0 +1,13 @@
+//
+//  IBConnection.h
+//  iotbroker.cloud-client
+//
+//  Created by MacOS on 01.12.16.
+//  Copyright © 2016 MobiusSoftware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IBConnection : NSObject
+
+@end
---
+++ iotbroker.cloud-client/IBConnection.m
@@ -0,0 +1,13 @@
+//
+//  IBConnection.m
+//  iotbroker.cloud-client
+//
+//  Created by MacOS on 01.12.16.
+//  Copyright © 2016 MobiusSoftware. All rights reserved.
+//
+
+#import "IBConnection.h"
+
+@implementation IBConnection
+
+@end
